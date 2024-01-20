9fa1ada6371371e1f4be5e28eedd934e01ee957f
Chipzstar <chipzstar.dev@googlemail.com>
2024-01-20 23:22:55 +0000

    add question 1 page

---
+++ apps/nextjs/src/app/utils/index.ts
@@ -0,0 +1,4 @@
+export const PATHS = {
+    HOME: '/',
+    QUESTION_1: '/what-is-your-one-goal'
+}
--- apps/nextjs/src/app/what-is-your-one-goal/page.tsx
+++ apps/nextjs/src/app/what-is-your-one-goal/page.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import {Button} from "@1goal/ui/button";
 import Image from "next/image";
+import {Progress} from "@1goal/ui/progress";
+import {Textarea} from "@1goal/ui/textarea";
 
 const page = () => {
     return (
-        <main className="flex items-center justify-center h-screen py-16">
+        <main className="flex items-center justify-center h-screen py-16 text-primary">
             <div className="absolute left-0 top-0">
                 <Image src="/images/logo-art.svg" alt="logo-art" width={500} height={500}/>
             </div>
-            <div className="flex flex-col items-center justify-center gap-4 space-y-6">
-                <h1 className="text-xl text-primary sm:text-[3rem] font-bold">
-                    What is your "ONE" goal?
+            <div className="flex flex-col items-center justify-center gap-4 space-y-6 z-20">
+                <Progress value={20} className="w-3/5 rounded-3xl mb-8"/>
+                <h1 className="text-xl sm:text-[3rem] font-bold">
+                    {'What is your "ONE" goal?'}
                 </h1>
-                <p className="text-3xl text-secondary">Life is a marathon, not a sprint</p>
+                <Textarea
+                    className="shadow-xl rounded-xl w-full"
+                    rows={4}
+                    placeholder="Please be specific and clear"
+                />
                 <Button size="lg" className="rounded-3xl h-16 w-144 shadow-xl">
-                    <span className="text-2xl">Create your one goal</span>
+                    <span className="text-3xl text-white">Enter one goal</span>
                 </Button>
-                <div className="w-full max-w-2xl overflow-y-scroll">
-                </div>
+                <span className="self-start">{"💡 What's your main goal - fitness, skill, personal growth, or other?"}</span>
+            </div>
+            <div className="absolute bottom-0 right-0">
+                <Image src="/images/mythical-animal-2.png" alt="mythical-beast-1" width={500} height={500}/>
             </div>
         </main>
     );
