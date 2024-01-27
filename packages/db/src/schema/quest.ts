5c24f9ae99e04ed46a14edcb9ae7a52680f0da81
batman <james@homer.local>
2024-01-27 18:07:16 +0000

    add quest table

---
+++ packages/db/src/schema/quest.ts
@@ -0,0 +1,25 @@
+import { relations, sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+type GoalParams = {
+  currentLevel: string;
+  timeline: string;
+  challengesFaced: string;
+  daysPerWeekAvailable: number;
+};
+
+export const quest = sqliteTable("quest", {
+  id: text("id").primaryKey(),
+  goal: text("goal").notNull(),
+  goalParams: text("goal_params", { mode: "json" })
+    .notNull()
+    // Making everything optional.
+    // Things could change in the future.
+    .$type<Partial<GoalParams>>(),
+  rawOpenAIResponse: text("raw_openai_response"),
+  openAIResponse: text("openai_response"),
+  isResponseValid: integer("is_response_valid", { mode: "boolean" }),
+  promptId: integer("prompt_id").notNull(),
+  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`),
+});
