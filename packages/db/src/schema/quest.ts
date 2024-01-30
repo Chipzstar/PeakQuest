7309f7eb1d9431f0bc01597e1fbcaae1c45f329e
jean-jacket <92271804+jean-jacket@users.noreply.github.com>
2024-01-30 10:11:57 +0000

    add character foreign key to quest table

--- packages/db/src/schema/quest.ts
+++ packages/db/src/schema/quest.ts
@@ -22,6 +22,8 @@ export const quest = sqliteTable("quest", {
   promptId: integer("prompt_id")
     .notNull()
     .references(() => prompt.id),
+  characterId: integer("character_id")
+    .references(() => character.id),
   createdBy: text("created_by").notNull(),
   createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
   updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`),
