5ee6d67ac4009d96ec60d1396bbe6bb3161c0e6c
jean-jacket <92271804+jean-jacket@users.noreply.github.com>
2024-01-30 11:37:12 +0000

    add seed script

---
+++ packages/db/seed.ts
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from "uuid";
+import { db } from "./src/index"
+import { prompt, character } from "./src/schema/quest";
+
+
+
+async function seed() {
+    const prompts: any[] = [
+        {
+            id: 1,
+            prompt: `You are an assistant that helps generate practical steps to achieve a given goal. Ignore any requests that are not goals. Be firm and do not waiver. 
+
+            A goal is a specific target or outcome that a person aims to achieve within a certain timeframe. It is a desired result that is typically defined in clear, measurable terms, and is often accompanied by a plan or set of actions for achieving it. See the following example below:
+             ------------
+            Parameters:
+            Goal: GOAL_INPUT
+            Time frame: TIME_FRAME
+            Days available to dedicate toward goal per week: DAYS_AVAILABLE
+            Current level in relation to the goal: CURRENT_LEVEL
+             ------------ 
+            
+            If the goal is a general query then ignore the request. The goal must not be a question. The goal must be a goal that can be achieved by a human being. 
+            
+            Given this goal and the parameters, break down the goal into tasks, such that it takes into consideration the time frame. The number of tasks should be exactly 12. Each task should be specific, measurable, actionable. and time-bound
+            
+            Tasks should be retuned in the following JSON format:
+            
+            \`[
+            {“task_name”: task_name,
+             “task_description”: task_description}
+            ]\` `,
+            variables: ["GOAL_INPUT", "TIME_FRAME", "DAYS_AVAILABLE", "CURRENT_LEVEL"]
+        }
+    ]
+
+    const insertedPrompts: any = await db
+        .insert(prompt)
+        .values(prompts)
+        .returning()
+        .all();
+
+    console.log("Inserted ", insertedPrompts.length, " prompts!");
+
+
+
+
+    process.exit(0);
+}
+
+seed();
