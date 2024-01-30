b054cd45acf92c78d1619e41d66194ae78991f1f
jean-jacket <92271804+jean-jacket@users.noreply.github.com>
2024-01-30 11:17:03 +0000

    make email unique

--- packages/db/src/schema/auth.ts
+++ packages/db/src/schema/auth.ts
@@ -3,7 +3,7 @@ import { integer, sqliteTable, text, primaryKey } from "drizzle-orm/sqlite-core"
 export const users = sqliteTable("user", {
     id: text("id").notNull().primaryKey(),
     name: text("name"),
-    email: text("email").notNull(),
+    email: text("email").notNull().unique(),
     emailVerified: integer("email_verified", { mode: "boolean" }),
     image: text("image"),
 })
