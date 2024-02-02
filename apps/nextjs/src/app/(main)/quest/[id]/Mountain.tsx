f8f222d8bbe84ce08d716f17ef2d8fc2455bfa9b
jean-jacket <92271804+jean-jacket@users.noreply.github.com>
2024-02-02 16:59:29 +0000

    make quest page server rendered

--- apps/nextjs/src/app/(main)/quest/[id]/Mountain.tsx
+++ apps/nextjs/src/app/(main)/quest/[id]/Mountain.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import React from 'react';
-import {MONSTERS, PATHS} from "~/app/utils";
+import { MONSTERS, PATHS } from "~/app/utils";
 import Monster from "~/app/components/Monster";
-import {Tooltip, TooltipArrow, TooltipContent, TooltipProvider, TooltipTrigger} from "@peakquest/ui/tooltip";
-import {useRouter, useSearchParams} from 'next/navigation';
+import { Tooltip, TooltipArrow, TooltipContent, TooltipProvider, TooltipTrigger } from "@peakquest/ui/tooltip";
+import { useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 
 // TODO: Adapt Quest page to use canvas API using KonvaJS
-const Quest = () => {
+const Mountain = () => {
     const router = useRouter()
     const searchParams = useSearchParams()
     const characterSrc = searchParams.get("src") ?? "/images/characters/character-1.svg"
@@ -34,11 +34,11 @@ const Quest = () => {
                         top: "87%",
                         left: "43%"
                     }}>
-                        <Image src={characterSrc} alt="Character" width={characterWidth} height={characterHeight}/>
+                        <Image src={characterSrc} alt="Character" width={characterWidth} height={characterHeight} />
                     </TooltipTrigger>
                     <TooltipContent>
                         <span>Change your Peak Quest Character</span>
-                        <TooltipArrow/>
+                        <TooltipArrow />
                     </TooltipContent>
                 </Tooltip>
             </TooltipProvider>
@@ -46,4 +46,4 @@ const Quest = () => {
     );
 };
 
-export default Quest;
+export default Mountain;
