1eae287ee988d36370e610cb9c7fba673cae26c7
jean-jacket <92271804+jean-jacket@users.noreply.github.com>
2024-02-02 13:40:45 +0000

    convert intro from page to standalone component

--- apps/nextjs/src/app/(get started)/confirm/page.tsx
+++ apps/nextjs/src/app/(get started)/confirm/page.tsx
@@ -2,8 +2,6 @@
 
 import React, { useEffect, useState } from 'react';
 import { Button } from "@peakquest/ui/button";
-import { PATHS } from "~/app/utils";
-import { useRouter } from 'next/navigation';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@peakquest/ui/form";
 import { useForm } from "react-hook-form";
@@ -17,96 +15,116 @@ import { useAtom } from 'jotai'
 import type { GoalState } from '~/app/lib/store';
 import { goalStateAtom } from '~/app/lib/store';
 import { toast } from '@peakquest/ui/toast';
+import Intro from '~/app/components/Intro';
+import { motion } from "framer-motion"
 
 const Confirm = () => {
-	const router = useRouter()
-	const [loading, setLoading] = useState(false);
-	const [goalState, setGoalState] = useAtom(goalStateAtom)
+    const [loading, setLoading] = useState(false);
+    const [questId, setQuestId] = useState("");
+    const [goalState, setGoalState] = useAtom(goalStateAtom)
 
-	const form = useForm<UserFormData>({
-		resolver: zodResolver(GettingStartedFormSchema),
-	})
+    const form = useForm<UserFormData>({
+        resolver: zodResolver(GettingStartedFormSchema),
+    })
 
-	async function onSubmit(data: UserFormData) {
-		setLoading(true)
-		try {
-			await saveGoal({ ...goalState as GoalState, name: data.name, email: data.email })
-			router.push(PATHS.INTRO)
-		} catch (err: any) {
-			toast("Something went wrong. Please try again.", {
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-				description: String(err.message),
-				duration: 3000
-			})
-		} finally {
-			setLoading(false)
-		}
-	}
+    async function onSubmit(data: UserFormData) {
+        setLoading(true)
+        try {
+            const id = await saveGoal({ ...goalState as GoalState, name: data.name, email: data.email })
+            setQuestId(id)
+        } catch (err: any) {
+            console.error(err)
+            toast("Something went wrong. Please try again.", {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                description: err.message,
+                duration: 3000
+            })
+        } finally {
+            setLoading(false)
+        }
+    }
 
-	useEffect(() => {
-		const storedValue = window.localStorage.getItem('user');
-		if (storedValue) {
-			try {
-				const user = JSON.parse(window.localStorage.getItem('user')!) as UserFormData
-				form.setValue('name', user.name, { shouldValidate: false });
-				form.setValue('email', user.email, { shouldValidate: false });
-			} catch (e) {
-				console.log('Failed to parse stored value');
-			}
-		}
-	}, []);
+    useEffect(() => {
+        // TODO: look into saving user id too
+        const storedValue = window.localStorage.getItem('user');
+        if (storedValue) {
+            try {
+                const user = JSON.parse(window.localStorage.getItem('user')!) as UserFormData
+                form.setValue('name', user.name, { shouldValidate: false });
+                form.setValue('email', user.email, { shouldValidate: false });
+            } catch (e) {
+                console.log('Failed to parse stored value');
+            }
+        }
+    }, []);
 
-	// Callback version of watch.  It's your responsibility to unsubscribe when done.
-	useEffect(() => {
-		const subscription = form.watch((value) => {
-			window.localStorage.setItem('user', JSON.stringify(value));
-		})
-		return () => subscription.unsubscribe();
-	}, [form.watch])
+    // Callback version of watch.  It's your responsibility to unsubscribe when done.
+    useEffect(() => {
+        const subscription = form.watch((value, { name, type }) => {
+            console.log(value, name, type)
+            window.localStorage.setItem('user', JSON.stringify(value));
+        })
+        return () => subscription.unsubscribe();
+    }, [form.watch])
 
-	return (
-		<main className="page-container justify-center bg-center bg-cover bg-no-repeat bg-mythical-beast">
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)}
-					  className="flex flex-col items-center justify-center gap-4 space-y-10">
-					<h1 className="text-4xl md:text-5xl lg:text-8xl text-zinc-600 text-center font-bold sm:mb-8">
-						Conquer your PEAK QUEST
-					</h1>
-					<FormField
-						control={form.control}
-						name="name"
-						render={({ field }) => (
-							<FormItem>
-								<FormControl>
-									<Input placeholder="First name" {...field} className="jumbo-input"/>
-								</FormControl>
-								<FormMessage/>
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="email"
-						render={({ field }) => (
-							<FormItem>
-								<FormControl>
-									<Input placeholder="Email Address" {...field} className="jumbo-input"/>
-								</FormControl>
-								<FormMessage/>
-							</FormItem>
-						)}
-					/>
-					<Button type="submit" size="lg" className="jumbo-button" disabled={loading}>
-						<Loader2 className={cn("text-white mr-3 sm:h-12 sm:w-12 animate-spin hidden", {
-							"block": loading,
-						})}/>
-						<span className="text-white">Get Started</span>
-					</Button>
-					<p className="text-xl md:text-4xl text-zinc-600 font-bold">Life is a marathon, not a sprint</p>
-				</form>
-			</Form>
-		</main>
-	);
+    if (questId !== "") {
+        return <motion.main
+            variants={{
+                hidden: { opacity: 0, x: 0, y: 0 },
+                enter: { opacity: 1, x: 0, y: 0 },
+            }}
+            initial="hidden"
+            animate="enter"
+            transition={{ duration: 2 }}
+        >
+            <Intro name={form.getValues("name")} questId={questId} />
+
+        </motion.main>
+    }
+
+    return (
+        <main className="page-container justify-center bg-center bg-cover bg-no-repeat bg-mythical-beast">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex flex-col items-center justify-center gap-4 space-y-10">
+                    <h1 className="text-4xl md:text-5xl lg:text-8xl text-zinc-600 text-center font-bold sm:mb-8">
+                        Conquer your PEAK QUEST
+                    </h1>
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input placeholder="First name" {...field} className="jumbo-input" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input placeholder="Email Address" {...field} className="jumbo-input" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" size="lg" className="jumbo-button" disabled={loading}>
+                        <Loader2 className={cn("text-white mr-3 sm:h-12 sm:w-12 animate-spin hidden", {
+                            "block": loading,
+                        })} />
+                        <span className="text-white">Get Started</span>
+                    </Button>
+                    <p className="text-xl md:text-4xl text-zinc-600 font-bold">Life is a marathon, not a sprint</p>
+                </form>
+            </Form>
+        </main>
+    );
 };
 
 export default Confirm;
