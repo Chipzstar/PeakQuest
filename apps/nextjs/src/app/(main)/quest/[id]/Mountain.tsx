4003063af5558d9e62529881d74c4be9e05d65d4
jean-jacket <92271804+jean-jacket@users.noreply.github.com>
2024-02-03 14:20:23 +0000

    update chosen character on ui

--- apps/nextjs/src/app/(main)/quest/[id]/Mountain.tsx
+++ apps/nextjs/src/app/(main)/quest/[id]/Mountain.tsx
@@ -1,29 +1,45 @@
 "use client"
 
 import React, { useState, useEffect } from 'react';
-import { MONSTERS, PATHS } from "~/app/utils";
+import { MONSTERS } from "~/app/utils";
 import Monster from "~/app/components/Monster";
 import { Tooltip, TooltipArrow, TooltipContent, TooltipProvider, TooltipTrigger } from "@peakquest/ui/tooltip";
-import { useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import SelectPlayer from '~/app/components/SelectPlayer';
+import { useAtom } from 'jotai'
+import { selectedCharacterAtom } from '~/app/lib/store';
+import { characters } from '~/app/components/SelectPlayer';
+
 
 interface MountainParams {
     questId?: string
     characterId?: number | null
 }
 
+const characterMap = new Map();
+
+characters.forEach((x) => {
+    characterMap.set(x.id, x)
+})
+
 // TODO: Adapt Quest page to use canvas API using KonvaJS
 const Mountain = (params: MountainParams) => {
     const hasNotSelectedCharacter = params.characterId == null
     const [showSelectCharacter, setShowSelectCharacter] = useState(hasNotSelectedCharacter)
-    const searchParams = useSearchParams()
-    const characterSrc = searchParams.get("src") ?? "/images/characters/character-1.svg"
-    const characterWidth = Number(searchParams.get("width") ?? 140)
-    const characterHeight = Number(searchParams.get("height") ?? 200)
+    const [selectedCharacter, setSelectedCharacter] = useAtom(selectedCharacterAtom)
+
+    useEffect(() => {
+        if (!hasNotSelectedCharacter) {
+            setSelectedCharacter(characterMap.get(params.characterId))
+        }
+    }, [])
+
+    const characterSrc = selectedCharacter?.src
+    const characterWidth = selectedCharacter?.width
+    const characterHeight = selectedCharacter?.height
 
 
-    if (showSelectCharacter) {
+    if (showSelectCharacter || (selectedCharacter == undefined)) {
         return <SelectPlayer questId={params.questId as string} setShowCharacter={setShowSelectCharacter} />
     }
 
@@ -44,11 +60,11 @@ const Mountain = (params: MountainParams) => {
                 <Tooltip>
                     <TooltipTrigger onClick={() => setShowSelectCharacter(true)} style={{
                         position: 'absolute',
-                        width: characterWidth / 3 ?? 100,
+                        width: characterWidth as number / 3 ?? 100,
                         top: "87%",
                         left: "43%"
                     }}>
-                        <Image src={characterSrc} alt="Character" width={characterWidth} height={characterHeight} />
+                        <Image src={characterSrc as string} alt="Character" width={characterWidth} height={characterHeight} />
                     </TooltipTrigger>
                     <TooltipContent>
                         <span>Change your Peak Quest Character</span>
