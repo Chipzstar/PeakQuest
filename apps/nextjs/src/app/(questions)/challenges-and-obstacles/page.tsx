7e467c4962d8a9e9023c04a2e01df6706503b658
Chipzstar <chipzstar.dev@googlemail.com>
2024-02-14 20:34:12 +0000

    remove max character limit from challenges text field

--- apps/nextjs/src/app/(questions)/challenges-and-obstacles/page.tsx
+++ apps/nextjs/src/app/(questions)/challenges-and-obstacles/page.tsx
@@ -18,94 +18,93 @@ import MythicalBeast5 from "~/app/assets/images/questions/mythical-animal-5.png"
 import type { StaticImport } from 'next/dist/shared/lib/get-img-props';
 
 const FormSchema = z.object({
-    challenges: z.union([z
-        .string()
-        .min(5, {
-            message: "Must be at least 5 characters.",
-        })
-        .max(30, {
-            message: "Must not be longer than 30 characters.",
-        }), z.literal("")])
+	challenges: z.union([z
+		.string()
+		.min(5, {
+			message: "Must be at least 5 characters.",
+		}), z.literal("")
+    ])
 })
 
 const Question4 = () => {
-    const router = useRouter()
-    const [goalState, setGoalState] = useAtom(goalStateAtom)
+	const router = useRouter()
+	const [goalState, setGoalState] = useAtom(goalStateAtom)
 
 
-    const form = useForm<z.infer<typeof FormSchema>>({
-        defaultValues: {
-            challenges: "",
-        },
-        resolver: zodResolver(FormSchema),
-    })
+	const form = useForm<z.infer<typeof FormSchema>>({
+		defaultValues: {
+			challenges: "",
+		},
+		resolver: zodResolver(FormSchema),
+	})
 
-    function onSubmit(data: z.infer<typeof FormSchema>) {
-        setGoalState({ ...goalState, challengesFaced: data.challenges })
-        router.push(PATHS.QUESTION_5)
-    }
+	function onSubmit(data: z.infer<typeof FormSchema>) {
+		setGoalState({ ...goalState, challengesFaced: data.challenges })
+		router.push(PATHS.QUESTION_5)
+	}
 
 
-    return (
-        <main className="page-container">
-            <div className="flex self-end md:absolute right-0 top-0 p-4">
-                <NextImage
-                    src={LogoArt as StaticImport}
-                    alt="hero"
-                    sizes="(max-width: 480px) 20vw, (max-width: 768px) 200vw, 150vw"
-                    style={{
-                        width: "100%",
-                        height: "auto",
-                        objectFit: "contain",
-                    }}
-                />
-            </div>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col items-center justify-center gap-4 space-y-6 lg:space-y-10 z-20 px-6">
-                    <div className="flex flex-col items-center">
-                        <Progress value={80} className="sm:w-96 md:h-7 rounded-3xl mb-8 w-2/3" />
-                        <h1 className="question-title sm:leading-tight lg:w-5/6">
-                            Are there any specific challenges or obstacles you anticipate facing while pursuing this
-                            goal?
-                        </h1>
-                    </div>
-                    <FormField
-                        control={form.control}
-                        name="challenges"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormControl>
-                                    <Textarea
-                                        className="jumbo-textarea"
-                                        rows={6}
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit" size="lg" className="jumbo-button">
-                        <span className="text-white">Enter challenges</span>
-                    </Button>
-                    <span
-                        className="question-hint">{"💡 What challenges do you expect? This shapes your checklist to overcome them."}</span>
-                </form>
-            </Form>
-            <div className="absolute bottom-0 left-0">
-                <NextImage
-                    src={MythicalBeast5}
-                    alt="mythical-beast-5"
-                    sizes="(max-width: 480px) 40vw, (max-width: 768px) 90vw, 175vw"
-                    style={{
-                        width: "100%",
-                        height: "auto",
-                        objectFit: "contain",
-                    }}
-                />
-            </div>
-        </main>
-    );
+	return (
+		<main className="page-container">
+			<div className="flex self-end md:absolute right-0 top-0 p-4">
+				<NextImage
+					src={LogoArt as StaticImport}
+					alt="hero"
+					sizes="(max-width: 480px) 20vw, (max-width: 768px) 200vw, 150vw"
+					style={{
+						width: "100%",
+						height: "auto",
+						objectFit: "contain",
+					}}
+				/>
+			</div>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}
+					  className="flex flex-col items-center justify-center gap-4 space-y-6 lg:space-y-10 z-20 px-6">
+					<div className="flex flex-col items-center">
+						<Progress value={80} className="sm:w-96 md:h-7 rounded-3xl mb-8 w-2/3"/>
+						<h1 className="question-title sm:leading-tight lg:w-5/6">
+							Are there any specific challenges or obstacles you anticipate facing while pursuing this
+							goal?
+						</h1>
+					</div>
+					<FormField
+						control={form.control}
+						name="challenges"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Textarea
+										className="jumbo-textarea"
+										rows={6}
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+					/>
+					<Button type="submit" size="lg" className="jumbo-button">
+						<span className="text-white">Enter challenges</span>
+					</Button>
+					<span
+						className="question-hint">{"💡 What challenges do you expect? This shapes your checklist to overcome them."}</span>
+				</form>
+			</Form>
+			<div className="absolute bottom-0 left-0">
+				<NextImage
+					src={MythicalBeast5}
+					alt="mythical-beast-5"
+					sizes="(max-width: 480px) 40vw, (max-width: 768px) 90vw, 175vw"
+					style={{
+						width: "100%",
+						height: "auto",
+						objectFit: "contain",
+					}}
+				/>
+			</div>
+		</main>
+	);
 };
 
 export default Question4;
